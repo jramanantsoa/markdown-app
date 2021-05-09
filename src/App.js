5e1cddef6de5dbfdc57955945381e7acde9acffd
Jessica <you@example.com>
2021-05-09 17:34:00 +0200

    added handleChange function which modifies the text in state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import { Component } from 'react';
 import { sampleText } from './sampleText'
 
 class App extends Component{
+  state = {
+    text:sampleText
+  }
+
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
   render() {
     return(
       <div className='container'>
         <div className = 'row'>
           <div className='col-sm-6'>
-            <textarea className='form-control' rows='35' value ={sampleText}></textarea>
+            <textarea
+              onChange ={this.handleChange}
+              value ={this.state.text }
+              className='form-control'
+              rows='35'
+            />
           </div>
           <div className='col-sm-6'>
-            { sampleText }
+            { this.state.text }
           </div>
         </div>
       </div>
